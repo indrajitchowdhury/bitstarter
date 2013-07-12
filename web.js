e706083bfbd74f7b343495132fe517501f78e1f0
Indrajit Chowdhury <indrajit@powerstores.com>
2013-07-12 03:21:22 +0000

    Now use a variable

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+  var buffer = 'monkey';
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
