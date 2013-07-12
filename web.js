d4bcf47add3c7cab5503573ea026138099c50e8c
Indrajit Chowdhury <indrajit@powerstores.com>
2013-07-12 16:12:25 +0000

    Added read of file buffer

--- web.js
+++ web.js
@@ -3,8 +3,8 @@ var fs = require('fs')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = 'monkey';
-  response.send(buffer);
+  var buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString()
 });
 
 var port = process.env.PORT || 5000;
